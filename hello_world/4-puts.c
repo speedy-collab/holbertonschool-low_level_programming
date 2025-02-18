7945590ab49b1b58bc4f00b9d936ac56c80f69d6
speedy-collab <10428@holbertonstudents.com>
2025-02-18 15:06:19 +0100

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ *main - main block
+ *Return: 0
+ *
+ */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
