842edbf289403137406547b1b1f1667e16167841
speedy-collab <10428@holbertonstudents.com>
2025-02-19 17:25:07 +0100

    test

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checking alphabetic characters
+ * Description: Write a function that computes the absolute value of an integer
+ * @r: The character to check
+ * Return: r if >= 0 , else -r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
