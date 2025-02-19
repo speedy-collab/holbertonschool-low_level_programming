dd961d840a8d33dc0b2481c4a4f1f60d9e24cae4
speedy-collab <10428@holbertonstudents.com>
2025-02-19 16:50:53 +0100

    test

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ * Description: Write a function that prints the alphabet
+ */
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
