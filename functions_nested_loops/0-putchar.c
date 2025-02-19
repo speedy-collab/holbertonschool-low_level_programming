726b31727c7a75a711f1ee2c0c8ab555c36c7cce
speedy-collab <10428@holbertonstudents.com>
2025-02-19 15:13:36 +0100

    test

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Write a program that prints _putchar.
+ * Return: Always 0 if success.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
