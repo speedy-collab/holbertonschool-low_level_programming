f15e54e15588a453c7299e9b1eeb28a7cf13efae
speedy-collab <10428@holbertonstudents.com>
2025-02-19 14:25:26 +0100

    test

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Write a program that prints the alphabet in lowercase
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet);
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
