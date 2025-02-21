dcb79ff0f17224ad309ae2abbafe3e0992763fd3
speedy-collab <10428@holbertonstudents.com>
2025-02-21 16:46:11 +0100

    jfrijedj

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (Failure)
- */
+* main - Entry point
+*
+* Return: Always 1 (Failure)
+*/
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return (1);
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, sizeof(message) - 1);
+return (1);
 }
