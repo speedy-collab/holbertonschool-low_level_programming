7bce1ec9f5860fbf1a7469fae61503a116747042
speedy-collab <10428@holbertonstudents.com>
2025-02-21 16:43:28 +0100

    rjke

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
     return (1);
 }
