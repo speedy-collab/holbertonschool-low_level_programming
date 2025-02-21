eafdbd2ace3d220f64c1e119905ab5ced2f4b49e
speedy-collab <10428@holbertonstudents.com>
2025-02-21 16:58:25 +0100

    hjsjksz

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
 * main - Entry point
-*
-* Return: Always 1 (Failure)
+* Description: This program prints a specific message to the standard error
+* followed by a new line, then returns 1.
+* Return: 1 (Failure)
 */
 int main(void)
 {
-message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
