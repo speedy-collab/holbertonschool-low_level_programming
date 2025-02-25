9ac33e071745bd0dbfcf99bc8a783726d119517f
speedy-collab <10428@holbertonstudents.com>
2025-02-25 14:58:56 +0100

    test

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,53 +1,53 @@
 #include <stdio.h>
 
 /**
- * _isdigit - Checks if the character is a digit (0-9)
- * @c: The character to check
- *
- * Return: 1 if c is a digit, 0 otherwise
- */
+* _isdigit - Checks if the character is a digit (0-9)
+* @c: The character to check
+*
+* Return: 1 if c is a digit, 0 otherwise
+*/
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
 
 /**
- * main - Test the _isdigit function
- *
- * Return: Always 0 (Success)
- */
+* main - Test the _isdigit function
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    /* Test the function using _putchar */
-    if (_isdigit('5'))
-    {
-        _putchar('1');  /* Should print 1 if it's a digit */
-    }
-    else
-    {
-        _putchar('0');  /* Should print 0 if it's not a digit */
-    }
+/* Test the function using _putchar */
+if (_isdigit('5'))
+{
+putchar('1');  /* Should print 1 if it's a digit */
+}
+else
+{
+putchar('0');  /* Should print 0 if it's not a digit */
+}
 
-    _putchar('\n');  /* Print newline */
+putchar('\n');  /* Print newline */
 
-    if (_isdigit('a'))
-    {
-        _putchar('1');  /* Should print 1 if it's a digit */
-    }
-    else
-    {
-        _putchar('0');  /* Should print 0 if it's not a digit */
-    }
+if (_isdigit('a'))
+{
+putchar('1');  /* Should print 1 if it's a digit */
+}
+else
+{
+putchar('0');  /* Should print 0 if it's not a digit */
+}
 
-    _putchar('\n');  /* Print newline */
+putchar('\n');  /* Print newline */
 
-    return (0);
+return (0);
 }
 
