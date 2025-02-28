7e45e5241f392bf8ebeb7307a3b69835e2f87820
speedy-collab <10428@holbertonstudents.com>
2025-02-28 15:17:13 +0100

    test

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* more_numbers - prints 10 times the numbers from 0 to 14, followed by a new line.
+*/
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+_putchar('1');  /* Print the tens place (1 for 10 to 14) */
+}
+
+_putchar(j % 10 + '0');  /* Print the ones place */
+}
+
+_putchar('\n');  /* Print a new line after each iteration */
+}
+}
