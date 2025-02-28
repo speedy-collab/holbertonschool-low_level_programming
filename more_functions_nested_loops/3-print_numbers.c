dfc589d4d830b702fc639edfc0d9d07f39660193
speedy-collab <10428@holbertonstudents.com>
2025-02-28 14:32:56 +0100

    test

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers from 0 to 9 followed by a new line.
+*/
+void print_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);  /* Print each number */
+}
+
+_putchar('\n');  /* Print a new line */
+}
+
