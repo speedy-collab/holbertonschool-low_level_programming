7a66db0cddf9548c15216583184e6f1e2a4da2f7
speedy-collab <10428@holbertonstudents.com>
2025-03-07 17:24:03 +0100

    test

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This function is main
+ * @argc: arguments contents
+ * @argv: arguments vectors
+ * Return: Voiture
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
