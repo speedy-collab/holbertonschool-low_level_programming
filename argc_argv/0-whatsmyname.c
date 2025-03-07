0bdd71adab459e41cdd03a90a7116ed9b43fdb74
speedy-collab <10428@holbertonstudents.com>
2025-03-07 17:10:14 +0100

    test

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This function is main
+ * @argc: arguments contents
+ * @argv: arguments vectors
+ * Return: Voiture
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
