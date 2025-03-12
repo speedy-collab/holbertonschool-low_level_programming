fe63adab59b319811b72f11de76811cc25fac768
speedy-collab <10428@holbertonstudents.com>
2025-03-12 14:06:19 +0100

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,31 @@
 #include "main.h"
-
 /**
-* _atoi - converts a string to an integer
-* @s: pointer to the string to be converted
-*
-* Description: This function converts the first occurrence of a number in the
-* string pointed to by s to an integer. It takes into account any preceding
-* '+' or '-' signs and handles situations where there are no numbers in the string.
-* If no numbers are found, the function returns 0.
-*
-* Return: the integer value of the first number found in the string,
-* or 0 if no number is found
-*/
+ * _atoi - Convert a string to an integer.
+ * @s: The string who need to be converted
+ * Return: The interger of the string or 0 if no numbers
+ */
 int _atoi(char *s)
-{
-int i = 0, sign = 1, num = 0;
 
-while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
 {
-if (s[i] == '-')
-{
-sign *= -1;
-}
-if (s[i] == '+' && !(s[i + 1] >= '0' && s[i + 1] <= '9'))
-{
-sign = 1;
-}
-i++;
-}
+	int i = 0;
+	int j = 1;
+	unsigned int k = 0;
 
-while (s[i] >= '0' && s[i] <= '9')
-{
-num = num * 10 + (s[i] - '0');
-i++;
-}
+	while (s[i] == ' ')
+		i++;
 
-return (num *sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			j *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			k = k * 10 + (s[i] - '0');
+		}
+		else if (k > 0)
+		break;
+		i++;
+	}
+
+	return (k * j);
 }
