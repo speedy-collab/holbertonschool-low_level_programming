ebe723357eb8572ad95e8019b517af0f15c80778
speedy-collab <10428@holbertonstudents.com>
2025-03-14 12:03:47 +0100

    test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,24 +2,25 @@
 #define DOG_H
 
 /**
- * struct dog - This is my struct for dogs
- * @name: First member
- * @age: Second member
- * @owner: Third member
- */
-
+* struct dog - A structure representing a dog
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*
+* Description: This structure contains information about a dog,
+* including its name, age, and owner.
+*/
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
+/* Define dog_t as a new name for struct dog */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG_H */
