b4712f37e0848ae4463c0280632c242ea1e4b750
speedy-collab <10428@holbertonstudents.com>
2025-03-19 17:38:14 +0100

    test

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
-* print_name - Prints a name using a callback function
+* print_name - Prints a name using a given function
 * @name: The name to be printed
-* @f: A pointer to a function that takes a char * as an argument
+* @f: A pointer to the function that will print the name
 *
-* Description: This function takes a name and a pointer to a function,
-* and it uses the function pointer to print the name in a specific way.
+* Description: This function takes a name and a function pointer as parameters.
+* It then calls the provided function, passing the name as an argument.
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
-
+if (name != NULL && f != NULL)
 f(name);
 }
