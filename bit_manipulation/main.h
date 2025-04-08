13ec0c6bf576abbabeed2e4ac87bcba28d0ae2f4
speedy-collab <10428@holbertonstudents.com>
2025-04-08 14:34:34 +0200

    test

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -8,5 +12,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
 
 #endif
